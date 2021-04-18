9b10590efcdd8b5257fa9c369c28c6b0852a0e6c
Zozime Kenji <kenji.zozime@insa-lyon.fr>
2021-04-18 14:19:27 +0200

    projet fini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bibliotheque';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBp1q_EnHtFV0NoJQPhHyR6gh4vQ5yRErE",
+      authDomain: "projet-bibliotheque-k.firebaseapp.com",
+      projectId: "projet-bibliotheque-k",
+      storageBucket: "projet-bibliotheque-k.appspot.com",
+      messagingSenderId: "72852524083",
+      appId: "1:72852524083:web:7d7b09eedf57901517a746"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
